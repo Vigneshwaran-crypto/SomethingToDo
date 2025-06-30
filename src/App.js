bced44caf73925fdb7df817aeb8f1e0a9ea81442
Vigneshwaran <vigneswaran@betamonks.com>
2025-06-30 20:44:36 +0530

    Initial Changes

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const callMe = () => {};
+
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      <button>Home</button>
+      <button>About</button>
+      <button>Settings</button>
+    </div>
+  );
+}
+
+export default App;
